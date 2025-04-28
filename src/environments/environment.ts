786c72866abb01eb95ab8bd996ca877ba60ae9b3
RaygozaO <oscar.raygoza@gmail.com>
2025-04-28 04:05:07 -0600

    agregando el enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   useCaptcha: false,
-  //apiBaseUrl: 'https://farmacia-mercurio.com/api', //cambiar a produccion
-  apiBaseUrl: 'http://mercurio.local/api',
+  apiBaseUrl: 'https://farmacia-mercurio.com/api', //cambiar a produccion
+  //apiBaseUrl: 'http://mercurio.local/api',
   endpoints: {
     login: '/auth/login',
     register: '/auth/register'
