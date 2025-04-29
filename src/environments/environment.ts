61718000e15935202c25305fc34e725e7ef79a36
RaygozaO <oscar.raygoza@gmail.com>
2025-04-29 16:34:15 -0600

    arreglando el jenkins

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   useCaptcha: false,
-  apiBaseUrl: 'https://farmacia-mercurio.com/api', //cambiar a produccion
+  apiBaseUrl: 'https://farmacia-mercurio.com/api',
   //apiBaseUrl: 'http://mercurio.local/api',
   endpoints: {
     login: '/auth/login',
